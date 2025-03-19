0cf1ef7951c9e8c2b41cb76e43ae31d546f4d7e8
Lu junru <lujunru31415926@163.com>
2025-03-19 17:09:36 +0800

    Update dpo_llama3.sh

--- training/dpo_llama3.sh
+++ training/dpo_llama3.sh
@@ -11,59 +11,20 @@ EPOCH=1
 SAVEINTERVAL=${EPOCH}
 LR=$(awk "BEGIN {print 4e-7 * sqrt($WORLD_SIZE)}")
 BETA=0.1
-DPOP_LAMBADA=50.0
-SFT_LAMBADA=1.0
-L1_LAMBADA=0.01
-TDPO_ALPHA=0.5
-NEFTALPHA=5.0
-SIMPO_LAMBADA=1.4
 LR_TYPE=linear
-
-NOISE=$1  # 0 for no noise and 1 for noise trails
-LNORM=$2  # 0 for no length norm and 1 for length norm
-NORMLOC=$3  # logps or logrs
-NORMTYPE=$4  # scale or top or random
-SEED=$5
-RLType=$6  # ipo or sigmoid or kto_pair or dpop or mix or l1 or orpo or tdpo
+SEED=42
+RLType=sigmoid
 ISLORA=0  # 1 for lora, 0 for full
-M_TYPE=llama  # default llama, llama / qwen / baichuan2
-
-DIRPRE=$7
-DIRPOST=$8
+M_TYPE=llama  # default llama, llama / qwen2
+PER_GPU_BATCH=4
+GRA_ACC=4
 
 raw_model_path=/row_model_path/
-train_data_path=./data/roleMRC_train-rl_halfmix.jsonl
+train_data_path=./data/roleMRC_train-rl_mix.jsonl
 eval_data_path=./data/roleMRC_dev-rl.jsonl
 deepspeed_config_path=./ds_config.json
-model_output_path=/output_path/${RLType}_llama3_1-8B-${DIRPOST}-half_mix/
+model_output_path=/output_path/${RLType}_llama3_1-8B-mix/
 cache_path=./cache
-
-if [ ${RLType} == "orpo" ]
-then
-    LR_TYPE=inverse_sqrt
-    LR=$(awk "BEGIN {print 4e-6 * sqrt($WORLD_SIZE)}")
-fi
-
-if [ ${RLType} == "simpo" ]
-then
-    BETA=2.5
-    EPOCH=1
-fi
-
-case ${raw_model_path} in 
-    *"8B"*)
-        PER_GPU_BATCH=4
-        GRA_ACC=4
-        ;;
-    *"7B"*)
-        PER_GPU_BATCH=4
-        GRA_ACC=4
-        ;;
-    *"70b"*)
-        PER_GPU_BATCH=1
-        GRA_ACC=16
-        ;;
-esac
 
 TRAINDATANUM=$(wc -l < "${train_data_path}")
 SAVESTEP=$(awk "BEGIN {print int(${TRAINDATANUM} * ${EPOCH} / (${PER_GPU_BATCH} * ${GRA_ACC} * $GPU_NUM_PER_NODE * ${SAVEINTERVAL} * $WORLD_SIZE)) + 1}")
@@ -109,16 +70,5 @@ CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun --nnodes=$WORLD_SIZE \
     --loss_type ${RLType} \
     --if_lora ${ISLORA} \
     --dpo_beta ${BETA} \
-    --add_noise ${NOISE} \
-    --len_norm ${LNORM} \
-    --lnorm_loc ${NORMLOC} \
-    --lnorm_type ${NORMTYPE} \
-    --neft_alpha ${NEFTALPHA} \
-    --dpop_lambda ${DPOP_LAMBADA} \
-    --sft_lambda ${SFT_LAMBADA} \
-    --l1_lambda ${L1_LAMBADA} \
-    --simpo_lambda ${SIMPO_LAMBADA} \
-    --tdpo_alpha ${TDPO_ALPHA} \
-    --model_type ${M_TYPE} \
     --cache_dir ${cache_path} \
     --seed ${SEED}
